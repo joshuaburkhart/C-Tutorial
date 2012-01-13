377c42d5443c18309477c182daed8ff89c24ed09
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-01-13 09:59:31 -0800

    finished chapter 1

---
+++ ch1/int_size.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+printf("size of a short is %d\n", (int)sizeof(short));
+printf("size of a int is %d\n", (int)sizeof(int));
+printf("size of a long is %d\n", (int)sizeof(long));
+}
